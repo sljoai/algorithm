d0466a7729c0cd553027a66b3e1cc8a9509091d0
songlaijiang <99026795@qq.com>
2017-10-23 18:06:54 +0800

    20171023-dp

---
+++ src/dp/LIS.java
@@ -0,0 +1,24 @@
+package dp;
+
+/**
+ * 最长递增子序列（Longest Increasing Sequence）:给定一个长度为N的数组A，计算A的最长的单调递增子序列（位置不一定连续）
+ * @author songlaijiang
+ *
+ */
+public class LIS {
+
+	public static void main(String[] args) {
+		
+	}
+	/**
+	 * 以当前索引作为递增序列的最后一个元素，查找能插入最大有效序列。若找到就在其基础上增1，否则以它作为子序列的第一个元素。
+	 * @param A
+	 * @return
+	 */
+	public static int LIS1(int A[]) {
+		int ret=0;
+		
+		return ret;
+	}
+
+}
